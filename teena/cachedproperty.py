1f889a37c568756676c2aef6665d2f23b94dd369
Zachary Voase <z@zacharyvoase.com>
2012-06-18 15:41:46 +0000

    Added a @cachedproperty decorator, with extensive unit tests.

---
+++ teena/cachedproperty.py
@@ -0,0 +1,55 @@
+import weakref
+
+__all__ = ['cachedproperty']
+
+
+class CachedProperty(object):
+
+    __slots__ = ('func', 'cache')
+
+    def __init__(self, func):
+        self.func = func
+        self.cache = weakref.WeakKeyDictionary()
+
+    def __get__(self, obj, type=None):
+        if obj is None:  # the property was accessed on the class.
+            return self
+        if obj not in self.cache:
+            self.cache[obj] = val = CachedPropertyValue(is_computed=True,
+                                                        value=self.func(obj))
+            return val.value
+        val = self.cache[obj]
+        if val.is_present:
+            if val.is_computed:
+                return val.value
+            val.value = result = self.func(obj)
+            val.is_computed = True
+            return result
+        raise AttributeError('%r has no attribute %r' %
+                             (obj, self.func.__name__))
+
+    def __set__(self, obj, value):
+        if obj not in self.cache:
+            self.cache[obj] = CachedPropertyValue(is_computed=True,
+                                                  value=value)
+        else:
+            val = self.cache[obj]
+            val.is_computed = True
+            val.value = value
+
+    def __delete__(self, obj):
+        self.cache[obj] = CachedPropertyValue(is_present=False)
+
+
+# An alias, for naming consistency with `property`.
+cachedproperty = CachedProperty
+
+
+class CachedPropertyValue(object):
+
+    __slots__ = ('is_present', 'is_computed', 'value')
+
+    def __init__(self, is_present=True, is_computed=False, value=None):
+        self.is_present = is_present
+        self.is_computed = is_computed
+        self.value = value
---
+++ test/test_cachedproperty.py
@@ -0,0 +1,158 @@
+import uuid
+
+from nose.tools import assert_raises
+
+from teena.cachedproperty import cachedproperty
+
+
+class Counter(object):
+    def __init__(self):
+        self.state = {'value': 0}
+
+    @cachedproperty
+    def attr(self):
+        self.state['value'] += 1
+        return self.state['value']
+
+
+def test_cached_property_is_computed_on_access():
+    counter = Counter()
+    initial_state = counter.state.copy()
+    value = counter.attr
+    final_state = counter.state.copy()
+    assert value == 1
+    assert initial_state['value'] == 0
+    assert final_state['value'] == 1
+
+
+def test_cached_property_is_only_computed_once():
+    counter = Counter()
+    assert counter.state['value'] == 0
+    first_value = counter.attr
+    assert first_value == 1
+    assert counter.state['value'] == 1
+    second_value = counter.attr
+    assert second_value == 1
+    assert counter.state['value'] == 1
+
+
+def test_setting_a_cached_property_overwrites_its_value_entirely():
+    # Access, set, access
+    counter = Counter()
+    first_value = counter.attr
+    assert first_value == 1
+    counter.attr = 123
+    second_value = counter.attr
+    assert second_value == 123
+    assert counter.state['value'] == 1  # Not recomputed.
+
+    # Set, access
+    counter2 = Counter()
+    counter2.attr = 123
+    value = counter2.attr
+    assert value == 123
+    assert counter2.state['value'] == 0  # Never computed.
+
+
+def test_deleting_a_cached_property_removes_it_from_the_instance():
+    # Access, delete, access
+    counter = Counter()
+    counter.attr
+    del counter.attr
+    assert_raises(AttributeError, lambda: counter.attr)
+    assert counter.state['value'] == 1  # Not recomputed.
+
+    # Delete, access
+    counter2 = Counter()
+    del counter2.attr
+    assert_raises(AttributeError, lambda: counter2.attr)
+    assert counter2.state['value'] == 0  # Never computed.
+
+
+def test_the_cachedproperty_descriptor_is_available_on_the_class():
+    assert isinstance(Counter.attr, cachedproperty)
+
+
+class MultiCounter(object):
+    def __init__(self):
+        self.state = {'foo': 0, 'bar': 0}
+
+    @cachedproperty
+    def foo(self):
+        self.state['foo'] += 1
+        return self.state['foo']
+
+    @cachedproperty
+    def bar(self):
+        self.state['bar'] += 1
+        return self.state['bar']
+
+
+def test_can_put_multiple_cached_properties_on_one_instance():
+    # Just a sanity check.
+    counter = MultiCounter()
+
+    assert counter.foo == 1
+    assert counter.state == {'foo': 1, 'bar': 0}
+
+    assert counter.bar == 1
+    assert counter.state == {'foo': 1, 'bar': 1}
+
+    counter.foo = 123
+    assert counter.foo == 123
+    assert counter.state == {'foo': 1, 'bar': 1}
+
+    counter.bar = 456
+    assert counter.bar == 456
+    assert counter.state == {'foo': 1, 'bar': 1}
+
+    del counter.foo
+    assert_raises(AttributeError, lambda: counter.foo)
+    assert counter.state == {'foo': 1, 'bar': 1}
+
+    del counter.bar
+    assert_raises(AttributeError, lambda: counter.bar)
+    assert counter.state == {'foo': 1, 'bar': 1}
+
+
+class ObjectWithIdentifier(object):
+
+    def __init__(self):
+        self.ident = str(uuid.uuid4())
+        self.deallocation_flag = [False]
+
+    def __eq__(self, other):
+        return (ObjectWithIdentifier, self.ident) == other
+
+    def __del__(self):
+        self.deallocation_flag[0] = True
+
+    @cachedproperty
+    def some_property(self):
+        return 123
+
+
+def test_objects_with_cached_properties_can_be_garbage_collected():
+    import gc
+    obj = ObjectWithIdentifier()
+    ident = obj.ident
+    dealloc_flag = obj.deallocation_flag
+
+    # Invoke the cachedproperty.
+    obj.some_property
+
+    # The object is tracked by the garbage collector.
+    assert any(tracked_obj == (ObjectWithIdentifier, ident)
+               for tracked_obj in gc.get_objects()), \
+            "The object is not being tracked by the garbage collector"
+
+    # Delete the object and run a full garbage collection.
+    del obj
+    gc.collect()
+
+    # The object is no longer tracked by the garbage collector.
+    assert not any(tracked_obj == (ObjectWithIdentifier, ident)
+                   for tracked_obj in gc.get_objects()), \
+            "The object is still being tracked by the garbage collector"
+    # The object has been deallocated.
+    assert dealloc_flag[0], "The object was not deallocated"
